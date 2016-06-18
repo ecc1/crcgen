23d93353ca50a02d38524cd70b5fee419a96854a
Eric Cooper <ecc@cmu.edu>
2016-06-18 19:19:52 -0400

    Use command-line flags for CRC size and polynomial

--- examples/crc16.go
+++ examples/crc16.go
@@ -1,11 +1,12 @@
 package main
 
-//go:generate ../crcgen/crcgen
+//go:generate ../crcgen/crcgen -size 16 -poly 0x1021
 
-func crc16(msg []byte) []byte {
+// Compute CRC-16 using CCITT polynomial.
+func crc16(msg []byte) uint16 {
 	res := uint16(0)
 	for _, b := range msg {
 		res = res<<8 ^ crc16Table[byte(res>>8)^b]
 	}
-	return MarshalUint16(res)
+	return res
 }
